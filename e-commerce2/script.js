003f6a434a30fb8a02dad2811c3c481736064f33
Arthur Menadel <menadelarthur@gmail.com>
2023-06-16 21:47:32 -0300

    ate la

--- e-commerce2/script.js
+++ e-commerce2/script.js
@@ -1,8 +1,8 @@
 
 /*=============== MENU SHOW Y HIDDEN ===============*/ 
 
-const navMenu = document.getElementById('nav-menu')
-      navToggle = document.getElementById('nav-toggle')  
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
 
@@ -34,3 +34,29 @@ const navMenu = document.getElementById('nav-menu')
             navMenu.classList.remove('show-menu')
         }
         navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+        /*=============== ACCORDION SKILLS ===============*/ 
+
+        const skillsContent = document.getElementsByClassName('skills__content'),
+              skillsHeader = document.querySelectorAll('.skills__header')
+
+              function toggleSkills(){
+                
+                let itemClass = this.parentNode.className
+
+                for(i = 0; i < skillsContent.length; i++)
+                {
+                  skillsContent[i].className = 'skills__content skills__close'
+                }
+                if(itemClass === 'skills__content skills__close')
+                {
+                  this.parentNode.className = 'skills__content skills__open'
+                }
+      
+              }
+
+              skillsHeader.forEach((el) =>
+              {
+                el.addEventListener('click', toggleSkills)
+              })
