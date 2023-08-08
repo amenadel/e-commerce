069cce583bcfeec757a92bc3a337444055e190ac
Arthur Menadel <menadelarthur@gmail.com>
2023-08-07 23:01:35 -0300

    consegui resolver

--- e-commerce4/assets/js/script.js
+++ e-commerce4/assets/js/script.js
@@ -1,6 +1,7 @@
 import ProductCard from "./ProductCard.js";
 import TrendingCard from "./TrendingCard.js";
 
+
 const navBar = document.querySelector(".header"),
     navBtn = document.querySelector(".header__btn"),
     sections = document.querySelectorAll("section[id]"),
@@ -31,15 +32,36 @@ function changeHeaderBg()
 /* Swiper JS */
 
 
+const homeSwiper = new Swiper(".home__content", {
+    loop: true,
+    effect: "fade",
+    speed: 2000,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+});
+
+homeSwiper.on("slideChange", () => {
+    const activeSlide = homeSwiper.slides[homeSwiper.activeIndex];
+    activeSlide.classList.add("reveal");
+});
+
+homeSwiper.on("slideChangeTransitionEnd", () => {
+    const prevSlide = homeSwiper.slides[homeSwiper.previousIndex];
+    prevSlide.classList.remove("reveal");
+});
+
+
 /* Circle Btn */
 
-    circleBtn = document.querySelector(".go-down-btn"),
-    circleText = circleBtn.querySelector(".circle-text");
+    
+let circleText = circleBtn.querySelector(".circle-text");
 circleText.innerHTML = circleText.textContent
-  .split("")
-  .map((char, index) => `<span style="transform: rotate(${index * 28.3}deg)">${char}</span>`)
-  .join("");
-
+    .split("")
+    .map((char, index) => `<span style="transform: rotate(${index * 28.3}deg)">${char}</span>`)
+    .join("");
 
 
 
@@ -77,14 +99,22 @@ circleText.innerHTML = circleText.textContent
 
 /* ============== Active Scroll ============== */
 
-
+function activeScroll()
+{
+}
 
 /* ============== Scroll Up ============== */
 
+function showScrollUpBtn() 
+{
+}
 
 
 /* ============== Call functions ============== */
 
 
+window.addEventListener("scroll", () => {});
 
-
+window.addEventListener("load", () => {
+    document.querySelector(".home__slide").classList.add("reveal")
+})
