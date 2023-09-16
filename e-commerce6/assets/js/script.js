19f3d8ea871bcf80d36c6d45c3d14a91580a36f7
Arthur Menadel <menadelarthur@gmail.com>
2023-09-16 00:12:39 -0300

    em progresso

--- e-commerce6/assets/js/script.js
+++ e-commerce6/assets/js/script.js
@@ -24,10 +24,10 @@ toastCloseBtn.addEventListener('click', function () {
 
 
 
-
+//Codigo original
 
 // mobile menu variables
-const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+/*const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
 const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
 const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
 const overlay = document.querySelector('[data-overlay]');
@@ -48,4 +48,70 @@ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
   mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
   overlay.addEventListener('click', mobileMenuCloseFunc);
 
+}
+*/
+
+//Codigo melhorado
+
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+
+  // mobile menu function
+  const mobileMenuCloseFunc = function () {
+    if (mobileMenu[i] && overlay) {
+      mobileMenu[i].classList.remove('active');
+      overlay.classList.remove('active');
+    }
+  }
+
+  mobileMenuOpenBtn[i].addEventListener('click', function () {
+    if (mobileMenu[i] && overlay) {
+      mobileMenu[i].classList.add('active');
+      overlay.classList.add('active');
+    }
+  });
+
+  if (mobileMenuCloseBtn[i]) {
+    mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+  }
+  
+  if (overlay) {
+    overlay.addEventListener('click', mobileMenuCloseFunc);
+  }
+}
+
+
+// accordion variables
+
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+
+for (let i = 0; i < accordionBtn.length; i++)
+{
+  accordionBtn[i].addEventListener('click', function () {
+
+    const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+    for (let i = 0; i < accordion.length; i++)
+    {
+      
+      if (clickedBtn) break;
+
+      if(accordion[i].classList.contains('active'))
+      {
+        accordion[i].classList.remove('active')
+        accordionBtn[i].classList.remove('active')
+      }
+
+    }
+
+    this.nextElementSibling.classList.toggle('active');
+
+    this.classList.toggle('active');
+
+  }) 
 }
